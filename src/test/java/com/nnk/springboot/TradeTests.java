df4777088175a5b362ef80c39055b2a41457e71b
Sarar Hammar <hammar.sarar@gmail.com>
2025-10-12 04:45:33 +0200

    test(trade): refactorisation des assertions dans le test Trade
    - Remplacement de `assertTrue(...equals(...))` par `assertEquals(expected, actual)`
    - Utilisation de `!listResult.isEmpty()` au lieu de `listResult.size() > 0` (plus lisible et performant)
    - Remplacement de `assertTrue(!xx)` par `assertFalse(xx)` pour plus de clarté logique

--- src/test/java/com/nnk/springboot/TradeTests.java
+++ src/test/java/com/nnk/springboot/TradeTests.java
@@ -2,19 +2,14 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.repositories.TradeRepository;
-//import org.junit.Assert; // JUnit 4
-import static org.junit.jupiter.api.Assertions.*; // JUnit 5
-//import org.junit.Test; // JUnit 4
-import org.junit.jupiter.api.Test; // JUnit 5
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//import org.junit.runner.RunWith; // JUnit 4
-//import org.springframework.test.context.junit4.SpringRunner; // JUnit 4
-import org.junit.jupiter.api.extension.ExtendWith; // JUnit 5
-import org.springframework.test.context.junit.jupiter.SpringExtension; // JUnit 5
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,16 +28,15 @@ public class TradeTests {
 		// Save
 		trade = tradeRepository.save(trade);
 		assertNotNull(trade.getTradeId());
-		assertTrue(trade.getAccount().equals("Trade Account"));
-
+		assertEquals("Trade Account", trade.getAccount()); //assertEquals(expected, actual);
 		// Update
 		trade.setAccount("Trade Account Update");
 		trade = tradeRepository.save(trade);
-		assertTrue(trade.getAccount().equals("Trade Account Update"));
+        assertEquals("Trade Account Update", trade.getAccount());
 
 		// Find
 		List<Trade> listResult = tradeRepository.findAll();
-		assertTrue(listResult.size() > 0);
+        assertFalse(listResult.isEmpty());
 
 		// Delete
 		Integer id = trade.getTradeId();
