6d7fcc2b2b69d77a529e819560d58aa4f701ce24
Sarar Hammar <hammar.sarar@gmail.com>
2025-11-27 17:55:20 +0100

    test(user): ajouter UserControllerIT avec couverture complète des endpoints CRUD
    - Création de la classe UserControllerIT avec tests d'intégration MockMvc
    - Couverture complète des routes : list, add, validate (success/erreur), update (GET/POST), delete
    - Ajout de @WithMockUser(ADMIN) pour anticiper la future configuration Spring Security
    - Vérification des vues, des redirections, des flash messages et des attributs du modèle
    - Validation des mises à jour et suppressions réelles en base via UserRepository
    - Alignement avec la structure et la qualité des tests des autres modules (Rating, Bid, CurvePoint, Rule)

---
+++ src/test/java/com/poseidon/tradingapp/controllers/UserControllerIT.java
@@ -0,0 +1,240 @@
+package com.poseidon.tradingapp.controllers;
+
+import com.poseidon.tradingapp.domain.User;
+import com.poseidon.tradingapp.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(username = "admin", roles = "ADMIN")
+public class UserControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setup() {
+        userRepository.deleteAll();
+    }
+
+    // ============================================================
+    // GET /user/list
+    // ============================================================
+    @Test
+    void shouldGetUserList() throws Exception {
+        // given
+        User user = new User();
+        user.setUsername("Cindy");
+        user.setFullname("Cindy Crawford");
+        user.setRole("USER");
+        user.setPassword("558899");
+
+        User saved = userRepository.save(user);
+
+        //when + then
+        mockMvc.perform(get("/user/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/list"))
+                .andExpect(model().attributeExists("users"))
+                .andExpect(model().attribute("users", hasSize(1)))
+        ;
+    }
+
+    // ============================================================
+    // GET /user/add
+    // ============================================================
+    @Test
+    void shouldShowAddFormUser() throws Exception {
+        //when + then
+        mockMvc.perform(get("/user/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/add"))
+                .andExpect(model().attributeExists("user"))
+        ;
+    }
+
+    // ============================================================
+    // POST user/validate (SUCCESS)
+    // ============================================================
+    @Test
+    void shouldAddUserWhenSuccess() throws Exception {
+        //when + then
+        mockMvc.perform(post("/user/validate")
+                        .param("fullname", "Jade BH")
+                        .param("username", "Jade")
+                        .param("role", "USER")
+                        .param("password", "654789")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("successMessage"))
+        ;
+    }
+
+    // ============================================================
+    // POST user/validate (FAILURE)
+    // ============================================================
+    @Test
+    void shouldNotAddUserWhenError() throws Exception {
+        //when + then
+        mockMvc.perform(post("/user/validate")
+                        .param("fullname", " Hammar")
+                        .param("username", "")// invalide
+                        .param("role", "USER")
+                        .param("password", "684719")
+                )
+                .andExpect(status().isOk()) // retour normal
+                .andExpect(view().name("user/add")) // retour au formulaire
+                .andExpect(model().attributeExists("user")) // DTO renvoyé à la vue, il contiendra les erreurs
+        ;
+    }
+
+    // ============================================================
+    // GET /user/update/{id} (SUCCESS)
+    // ============================================================
+    @Test
+    void shouldDisplayUpdateUserForm() throws Exception {
+
+        User user = new User();
+        user.setUsername("cindy");
+        user.setFullname("Cindy Crawford");
+        user.setRole("USER");
+        user.setPassword("1234");
+
+        User saved = userRepository.save(user);
+
+        mockMvc.perform(get("/user/update/" + saved.getUserId()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/update"))
+                .andExpect(model().attributeExists("user"));
+    }
+
+    // ============================================================
+    // GET /user/update/{id} (NOT FOUND)
+    // ============================================================
+    @Test
+    void shouldRedirectUpdateFormWhenUserNotFound() throws Exception {
+
+        mockMvc.perform(get("/user/update/999"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("errorMessage"));
+    }
+
+    // ============================================================
+    // POST /user/update/{id} (SUCCESS)
+    // ============================================================
+    @Test
+    void shouldUpdateUserWhenSuccess() throws Exception {
+
+        User user = new User();
+        user.setUsername("cindy");
+        user.setFullname("Cindy Crawford");
+        user.setRole("USER");
+        user.setPassword("1234");
+        User saved = userRepository.save(user);
+
+        mockMvc.perform(post("/user/update/" + saved.getUserId())
+                        .param("username", "Cino")
+                        .param("fullname", "Cindy Updated")
+                        .param("role", "ADMIN")
+                        .param("password", "9999"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("successMessage"));
+
+        User updated = userRepository.findById(saved.getUserId()).orElseThrow();
+        assertEquals("Cino", updated.getUsername());
+        assertEquals("Cindy Updated", updated.getFullname());
+        assertEquals("ADMIN", updated.getRole());
+    }
+
+
+    // ============================================================
+    // POST /user/update/{id} (ERROR VALIDATION)
+    // ============================================================
+    @Test
+    void shouldReturnToUpdateFormOnValidationError() throws Exception {
+
+        User user = new User();
+        user.setUsername("cindy");
+        user.setFullname("Cindy");
+        user.setRole("USER");
+        user.setPassword("1234");
+        User saved = userRepository.save(user);
+
+        mockMvc.perform(post("/user/update/" + saved.getUserId())
+                        .param("username", "") // invalide
+                        .param("fullname", "Cindy")
+                        .param("role", "USER")
+                        .param("password", "1234"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/update"))
+                .andExpect(model().attributeExists("user"));
+    }
+
+
+    // ============================================================
+    // POST /user/update/{id} (NOT FOUND)
+    // ============================================================
+    @Test
+    void shouldRedirectUpdateWhenUserNotFound() throws Exception {
+
+        mockMvc.perform(post("/user/update/999")
+                        .param("username", "test")
+                        .param("fullname", "Test Test")
+                        .param("role", "USER")
+                        .param("password", "1234"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("errorMessage"));
+    }
+
+    // ============================================================
+    // GET /user/delete/{id} (SUCCESS)
+    // ============================================================
+    @Test
+    void shouldDeleteUser() throws Exception {
+
+        User user = new User();
+        user.setUsername("cindy");
+        user.setFullname("Cindy Crawford");
+        user.setRole("USER");
+        user.setPassword("1234");
+        User saved = userRepository.save(user);
+
+        mockMvc.perform(get("/user/delete/" + saved.getUserId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("successMessage"));
+
+        assertEquals(0, userRepository.count());
+    }
+
+    // ============================================================
+    // GET /user/delete/{id} (NOT FOUND)
+    // ============================================================
+    @Test
+    void shouldRedirectDeleteWhenUserNotFound() throws Exception {
+
+        mockMvc.perform(get("/user/delete/999"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/list"))
+                .andExpect(flash().attributeExists("errorMessage"));
+    }
+}
